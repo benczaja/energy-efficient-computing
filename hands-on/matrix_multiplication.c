0bfdc9b97eb54ef098c9ac195ff703adbd2dbe80
Benjamin Czaja <benjaminczaja@gmail.com>
2023-05-02 10:38:38 +0200

    Can now initilize large matricies (maybe too large)

--- hands-on/matrix_multiplication.c
+++ hands-on/matrix_multiplication.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<inttypes.h>
 #include<time.h>
+#include<string.h>
 
 int main( int argc, char *argv[] )  {
 
@@ -17,54 +17,51 @@ int main( int argc, char *argv[] )  {
         exit(1);
     }
     srand(time(NULL)); // Initialization or RANDOM NUMBER
-    long int x_tmp;
-    long int y_tmp;
-    char *eptr;
+    int x_size;
+    int y_size;
+    sscanf(argv[1], "%d", &x_size);
+    sscanf(argv[2], "%d", &y_size);
 
-    x_tmp = strtol(argv[1],&eptr,10);
-    y_tmp = strtol(argv[2],&eptr,10);
+    // This is probably overkill
+    float (*A)[6];
+    float (*B)[6];
+    float (*C)[6];
+    A = malloc(sizeof *A * 1000000);
+    B = malloc(sizeof *B * 1000000);
+    C = malloc(sizeof *C * 1000000);
 
-    int x_size = (int)x_tmp;
-    int y_size = (int)y_tmp;
-
-    printf("Initialized x_size,y_size\n");
-
-    int a[x_size][y_size];
-    int b[x_size][y_size];
-    int c[x_size][y_size];
-
-    printf("Initialized a,b,c\n");
-
-    // loop an init with rand vals
+    printf("Initializing A and B arrays with random values (1..10)\n");
     for(int i=0;i<x_size;i++)
     {
         for(int j=0;j<y_size;j++)
         {
-            a[i][j] = rand() % 10; // Get a number between 1 and 10
-            b[i][j] = rand() % 10; // Get a number between 1 and 10
+            A[i][j] = rand() % 10; // Get a number between 1 and 10
+            B[i][j] = rand() % 10; // Get a number between 1 and 10
         }
     }
 
+
+    printf("Multiplying A*B\n");
     for(int i=0;i<x_size;i++)
     {
         for(int j=0;j<y_size;j++)
         {
-            c[i][j]=0;
+            C[i][j]=0;
             for(int k=0;k<y_size;k++)
             {
-                c[i][j]+=a[i][k]*b[k][j];
+                C[i][j] += A[i][k]*B[k][j];
             }
         }
     }
-    //for printing result
-    printf("The result of a*b = \n");
-    for(int i=0;i<x_size;i++)
-    {
-        for(int j=0;j<y_size;j++)
-        {
-            //printf("%d\t",c[i][j]);
-        }
-        //printf("\n");
-    }
-    return 0;
+    ////for printing result
+    //printf("The result of a*b = \n");
+    //for(int i=0;i<x_size;i++)
+    //{
+    //    for(int j=0;j<y_size;j++)
+    //    {
+    //        //printf("%d\t",c[i][j]);
+    //    }
+    //    //printf("\n");
+    //}
+    //return 0;
 }
