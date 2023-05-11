aeb479d1a049074f908a96d180012da0e9117b72
Benjamin Czaja <benjamic@int6.local.snellius.surf.nl>
2023-05-11 16:33:12 +0200

    WIP

---
+++ hands-on/monitoring/energy_monitoring_study.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+FILE=results.txt
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    echo "Removing $FILE"
+    rm $FILE
+fi
+
+
+for i in {0..10..1}
+do
+    echo "Running $i $i"
+    ../src/mat_mul_pmt_v2 $i $i > templog.txt
+
+    #just some dumb commands to "log the results"
+    Size=$(sed -n '/Matrix Size:/p' templog.txt | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+    Time=$(sed -n '/PMT Seconds:/p' templog.txt | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+    Watt=$(sed -n '/PMT Watts:/p' templog.txt | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+    Joule=$(sed -n '/PMT Joules:/p' templog.txt | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+    
+    echo $Size $Time $Joule $Watt >> results.txt
+
+done
