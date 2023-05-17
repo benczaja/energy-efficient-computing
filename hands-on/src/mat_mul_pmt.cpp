640f6edb7d52b105c2cd3a095b927db6cdf88b74
Benjamin Czaja <benjamic@int5.local.snellius.surf.nl>
2023-05-17 11:08:50 +0200

    Updated mat_mul_pmt

--- hands-on/src/mat_mul_pmt.cpp
+++ hands-on/src/mat_mul_pmt.cpp
@@ -1,18 +1,43 @@
-#include<stdio.h> // needed for ‘printf’ 
-#include<stdlib.h> // needed for ‘RAND_MAX’ 
-#include<time.h> // needed for clock() and CLOCKS_PER_SEC etc
-#include<omp.h> // needed for OpenMP 
-#include<iostream> // needed for cout, endl etc etc
-#include<pmt.h> // needed for PMT
+#include <stdio.h> // needed for ‘printf’ 
+#include <stdlib.h> // needed for ‘RAND_MAX’ 
+#include <string.h> // needed for strcmp
+#include <stdbool.h> // needed for bool usage
+#include <ctype.h> // needed for isdigit
+#include <omp.h> // needed for OpenMP 
+#include <time.h> // needed for clock() and CLOCKS_PER_SEC etc
 
-static const int ROWS = 2000;     // Number of rows in each matrix
-static const int COLUMNS = 2000;  // Number of columns in each matrix
+// THIS IS NEW!!!!!!
+#include <pmt.h> // needed for PMT
+#include<iostream> // needed for CPP IO ... cout, endl etc etc
 
-float A[ROWS][COLUMNS];    // Matrix A
-float B[ROWS][COLUMNS];    // Matric B
-float C[ROWS][COLUMNS];    // result
 
-void initialize_matrices(){
+void printUsage()
+{
+    fprintf(stderr, "mat_mul (matrix size) [-s|-p|-h]\n");
+    fprintf(stderr, "\t      Invoke simple implementation of matrix multiplication\n");
+    fprintf(stderr, "\t-s    Invoke simple implementation of matrix multiplication\n");
+    fprintf(stderr, "\t-p    Invoke parallel (OpenMP) implementation of matrix multiplication\n");
+    fprintf(stderr, "\t-h    Display help\n");
+}
+
+bool isNumber(char number[])
+{
+    int i = 0;
+
+    //checking for negative numbers
+    if (number[0] == '-')
+        i = 1;
+    for (; number[i] != 0; i++)
+    {
+        //if (number[i] > '9' || number[i] < '0')
+        if (!isdigit(number[i]))
+            return false;
+    }
+    return true;
+}
+
+
+void initialize_matrices(float** A, float** B, float** C, int ROWS, int COLUMNS){
     for (int i = 0 ; i < ROWS ; i++)
     {
         for (int j = 0 ; j < COLUMNS ; j++)
@@ -24,8 +49,10 @@ void initialize_matrices(){
     }
 }
 
-void simple_matrix_multiply(){
-    printf("(Simple) Matix Multiplication\n");
+void simple_matrix_multiply(float** A, float** B, float** C, int ROWS, int COLUMNS){
+    
+    printf("(Simple) Matix Multiplication of 2D matricies of equal sizes (%d, %d)\n",ROWS,COLUMNS);
+
     for(int i=0;i<ROWS;i++)
     {
         for(int j=0;j<COLUMNS;j++)
@@ -38,9 +65,12 @@ void simple_matrix_multiply(){
     }
 }
 
-void openmp_matrix_multiply(){
+void openmp_matrix_multiply(float** A, float** B, float** C, int ROWS, int COLUMNS){
+    
     int num_threads = omp_get_max_threads();
-    printf("(OpenMP) Matix Multiplication Using %d Threads\n", num_threads);
+    
+    printf("(OpenMP) Matix Multiplication of 2D matricies of equal sizes (%d, %d)\n",ROWS,COLUMNS);
+    printf("Using %d Threads\n", num_threads);
     
     #pragma omp parallel for 
     for (int i = 0; i < ROWS; ++i) 
@@ -55,33 +85,126 @@ void openmp_matrix_multiply(){
     }
 }
 
-int main()  {
+
+int main( int argc, char *argv[] )  {
+
+  int ROWS;
+  int COLUMNS;
+
+  /* bools needed for the argument parsing logic */
+  bool openmp = false;
+  bool simple = true;
+  bool sanity_check = false;
+
+  /* Parse the arguments */
+  for(int i=0; i<argc; ++i)
+  {   
+      if (! strcmp("-s", argv[i]))
+      {
+          simple = true;
+      }
+      else if (! strcmp("-p", argv[i]))
+      {
+          openmp = true;
+          simple = false;
+      }
+      else if (!strcmp("-h", argv[i]))
+      {
+          printUsage();
+          return (EXIT_FAILURE) ;
+      }
+      else if (isNumber(argv[i])){
+        sscanf(argv[1], "%d", &ROWS);
+        sscanf(argv[1], "%d", &COLUMNS);
+        sanity_check = true;
+      }
+  }
+  /* Dumb logic to make sure an array size was passed */
+  if (! sanity_check){
+      printf("I need (arraysize) as an argument ..... Exiting.\n");
+      return (EXIT_FAILURE);
+  }
+
+  /* declare the arrays */
+  float **A;
+  float **B;
+  float **C;
+
+  /* allocate the arrays */
+  A = malloc(ROWS * sizeof *A);
+  B = malloc(ROWS * sizeof *B);
+  C = malloc(ROWS * sizeof *C);
+  for (int i=0; i<ROWS; i++)
+  {
+    A[i] = malloc(COLUMNS * sizeof *A[i]);
+    B[i] = malloc(COLUMNS * sizeof *B[i]);
+    C[i] = malloc(COLUMNS * sizeof *C[i]);
+  }
+
+  /*======================================================================*/
+  /*                START of Section of the code that matters!!!          */
+  /*======================================================================*/
+
+  /* initialize the arrays */
+  initialize_matrices(A, B, C, ROWS, COLUMNS);
+
+    // THIS IS NEW !!!!!!!
     auto sensor = pmt::rapl::Rapl::create();
+
+  /* Simple matrix multiplication */
+  /*==============================*/
+  if (true == simple)
+  {
+    //Start the PMT "sensor"
     auto start = sensor->read();
+    
+    simple_matrix_multiply(A, B, C, ROWS, COLUMNS);
 
-    printf("Initializing A (size %d) and B (size %d) arrays with random values between (0..1)\n",ROWS,COLUMNS);
-    initialize_matrices();
-
-    simple_matrix_multiply(); // Do the matrix multiplication
+    //End the PMT "sensor"
     auto end = sensor->read();
 
+    /// SORRY FOR THE CPP !!!!! BUT WE ARE JUST PRINTING!!!!
     std::cout<<"RESULTS-------"<<std::endl;
     std::cout<<"Matrix Size: "<<ROWS<<std::endl;
     std::cout <<"PMT Seconds: "<< pmt::PMT::seconds(start, end) << " s"<< std::endl;
     std::cout <<"PMT Joules: " << pmt::PMT::joules(start, end) << " J" << std::endl;
     std::cout <<"PMT Watts: " << pmt::PMT::watts(start, end) << " W" << std::endl;
 
-    
-    
-    //initialize_matrices(); // We need to re-intiaize to make sure we get "correct answer"
+  }
 
-    //start = sensor->read();
-    //openmp_matrix_multiply(); // Do the matrix multiplication
-    //end = sensor->read();
 
-    //std::cout <<"PMT seconds: "<< pmt::PMT::seconds(start, end) << " s"<< std::endl;
-    //std::cout <<"PMT Joules: " << pmt::PMT::joules(start, end) << " J" << std::endl;
-    //std::cout <<"PMT Watts: " << pmt::PMT::watts(start, end) << " W" << std::endl;
+  /* OpenMP parallel matrix multiplication */
+  /*=======================================*/
+  if (true == openmp)
+  {
+    //Start the PMT "sensor"
+    auto start = sensor->read();
     
-    return 0;
+    openmp_matrix_multiply(A, B, C, ROWS, COLUMNS);
+
+    //End the PMT "sensor"
+    auto end = sensor->read();
+
+    /// SORRY FOR THE CPP !!!!! BUT WE ARE JUST PRINTING!!!!
+    std::cout<<"RESULTS-------"<<std::endl;
+    std::cout<<"Matrix Size: "<<ROWS<<std::endl;
+    std::cout <<"PMT Seconds: "<< pmt::PMT::seconds(start, end) << " s"<< std::endl;
+    std::cout <<"PMT Joules: " << pmt::PMT::joules(start, end) << " J" << std::endl;
+    std::cout <<"PMT Watts: " << pmt::PMT::watts(start, end) << " W" << std::endl;
+  }
+
+  /*======================================================================*/
+  /*                 END of Section of the code that matters!!!           */
+  /*======================================================================*/
+
+  /* deallocate the arrays */
+  for (int i=0; i<ROWS; i++)
+  {
+    free(A[i]);
+    free(B[i]);
+    free(C[i]);
+  }
+  free(A);
+  free(B);
+  free(C);
 }
