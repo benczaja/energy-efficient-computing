7573ee0b07c79fafe99f838bcda0b8d7b4e78b24
Benjamin Czaja <benjamic@int4.local.snellius.surf.nl>
2023-05-30 18:08:52 +0200

    WIP

---
+++ hands-on/install_examples.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Building Examples..."
+cd src 
+
+module purge
+module load 2022
+module load foss/2022a
+
+gcc -fopenmp mat_mul.c -o ../bin/mat_mul
+gcc -fopenmp saxpy.c -o ../bin/saxpy
+
+module load pmt/1.1.0-foss-2022a
+
+g++ -fopenmp -lpmt -fpermissive mat_mul_pmt.cpp -o ../bin/mat_mul_pmt
+
+echo "Executables can be found in src"
--- hands-on/src/mat_mul_pmt.cpp
+++ hands-on/src/mat_mul_pmt.cpp
@@ -1,40 +1,12 @@
 #include <stdio.h> // needed for ‘printf’ 
 #include <stdlib.h> // needed for ‘RAND_MAX’ 
-#include <string.h> // needed for strcmp
-#include <stdbool.h> // needed for bool usage
-#include <ctype.h> // needed for isdigit
 #include <omp.h> // needed for OpenMP 
 #include <time.h> // needed for clock() and CLOCKS_PER_SEC etc
+#include "helper.h" // local helper header to clean up code
 
 // THIS IS NEW!!!!!!
 #include <pmt.h> // needed for PMT
 #include<iostream> // needed for CPP IO ... cout, endl etc etc
-
-
-void printUsage()
-{
-    fprintf(stderr, "mat_mul (matrix size) [-s|-p|-h]\n");
-    fprintf(stderr, "\t      Invoke simple implementation of matrix multiplication\n");
-    fprintf(stderr, "\t-s    Invoke simple implementation of matrix multiplication\n");
-    fprintf(stderr, "\t-p    Invoke parallel (OpenMP) implementation of matrix multiplication\n");
-    fprintf(stderr, "\t-h    Display help\n");
-}
-
-bool isNumber(char number[])
-{
-    int i = 0;
-
-    //checking for negative numbers
-    if (number[0] == '-')
-        i = 1;
-    for (; number[i] != 0; i++)
-    {
-        //if (number[i] > '9' || number[i] < '0')
-        if (!isdigit(number[i]))
-            return false;
-    }
-    return true;
-}
 
 
 void initialize_matrices(float** A, float** B, float** C, int ROWS, int COLUMNS){
@@ -90,41 +62,17 @@ int main( int argc, char *argv[] )  {
 
   int ROWS;
   int COLUMNS;
+  int N;
 
-  /* bools needed for the argument parsing logic */
+  /* DUMB bools needed for the argument parsing logic */
   bool openmp = false;
   bool simple = true;
   bool sanity_check = false;
-
-  /* Parse the arguments */
-  for(int i=0; i<argc; ++i)
-  {   
-      if (! strcmp("-s", argv[i]))
-      {
-          simple = true;
-      }
-      else if (! strcmp("-p", argv[i]))
-      {
-          openmp = true;
-          simple = false;
-      }
-      else if (!strcmp("-h", argv[i]))
-      {
-          printUsage();
-          return (EXIT_FAILURE) ;
-      }
-      else if (isNumber(argv[i])){
-        sscanf(argv[1], "%d", &ROWS);
-        sscanf(argv[1], "%d", &COLUMNS);
-        sanity_check = true;
-      }
-  }
-  /* Dumb logic to make sure an array size was passed */
-  if (! sanity_check){
-      printf("I need (arraysize) as an argument ..... Exiting.\n");
-      return (EXIT_FAILURE);
-  }
-
+  
+  /* VERY DUMB Argument Parsers */
+  N = parse_arguments(argc, argv, &simple, &openmp, &sanity_check);
+  ROWS = N;
+  COLUMNS = N;
   /* declare the arrays */
   float **A;
   float **B;
