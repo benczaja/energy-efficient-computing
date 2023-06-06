fecae0c113afd390b365cb567b80e387659f23ec
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-06-06 13:28:31 +0200

    Formatting source code.

--- hands-on/KT/common.py
+++ hands-on/KT/common.py
@@ -5,21 +5,23 @@ import pycuda.driver as drv
 import numpy as np
 import math
 import kernel_tuner
+
 drv.init()
 
 from kernel_tuner.nvml import nvml
 
 
 def get_device_name(device):
-    return drv.Device(device).name().replace(' ', '_')
+    return drv.Device(device).name().replace(" ", "_")
+
 
 def get_fallback():
-    if os.uname()[1].startswith('node0'):
+    if os.uname()[1].startswith("node0"):
         return "/cm/shared/package/utils/bin/run-nvidia-smi"
     return "nvidia-smi"
 
-def get_metrics(total_flops):
 
+def get_metrics(total_flops):
     metrics = OrderedDict()
     metrics["GFLOP/s"] = lambda p: total_flops / (p["time"] / 1000.0)
     metrics["GFLOPS/W"] = lambda p: total_flops / p["nvml_energy"]
@@ -41,7 +43,8 @@ def get_pwr_limits(device, n=None):
 
     # Rounded power limit values
     power_limits = power_limit_round * np.round(
-        (np.linspace(power_limit_min, power_limit_max, n) / power_limit_round))
+        (np.linspace(power_limit_min, power_limit_max, n) / power_limit_round)
+    )
     power_limits = list(set([int(power_limit) for power_limit in power_limits]))
     tune_params["nvml_pwr_limit"] = power_limits
     print("Using power limits:", tune_params["nvml_pwr_limit"])
@@ -53,7 +56,7 @@ def get_supported_mem_clocks(device, n=None):
     mem_clocks = d.supported_mem_clocks
 
     if n and len(mem_clocks) > n:
-        mem_clocks = mem_clocks[::int(len(mem_clocks)/n)]
+        mem_clocks = mem_clocks[:: int(len(mem_clocks) / n)]
 
     tune_params = OrderedDict()
     tune_params["nvml_mem_clock"] = mem_clocks
@@ -67,7 +70,7 @@ def get_gr_clocks(device, n=None):
     gr_clocks = d.supported_gr_clocks[mem_clock]
 
     if n and (len(gr_clocks) > n):
-        gr_clocks = gr_clocks[::math.ceil(len(gr_clocks)/n)]
+        gr_clocks = gr_clocks[:: math.ceil(len(gr_clocks) / n)]
 
     tune_params = OrderedDict()
     tune_params["nvml_gr_clock"] = gr_clocks[::-1]
@@ -76,9 +79,14 @@ def get_gr_clocks(device, n=None):
 
 
 def get_default_parser():
-    parser = argparse.ArgumentParser(description='Tune kernel')
+    parser = argparse.ArgumentParser(description="Tune kernel")
     parser.add_argument("-d", dest="device", nargs="?", default=0, help="GPU ID to use")
-    parser.add_argument("-f", dest="overwrite", action="store_true", help="Overwrite any existing .json files")
+    parser.add_argument(
+        "-f",
+        dest="overwrite",
+        action="store_true",
+        help="Overwrite any existing .json files",
+    )
     parser.add_argument("--suffix", help="Suffix to append to output file names")
     parser.add_argument("--tune-power-limit", action="store_true")
     parser.add_argument("--power-limit-steps", nargs="?")
@@ -91,4 +99,5 @@ def report_most_efficient(results, tune_params, metrics):
     best_config = min(results, key=lambda x: x["nvml_energy"])
     print("most efficient configuration:")
     kernel_tuner.util.print_config_output(
-        tune_params, best_config, quiet=False, metrics=metrics, units=None)
+        tune_params, best_config, quiet=False, metrics=metrics, units=None
+    )
--- hands-on/KT/tuning_gemm.py
+++ hands-on/KT/tuning_gemm.py
@@ -17,37 +17,42 @@ from common import *
 
 unit = "GFLOP"
 
+
 def ops(m, n, k):
-    return (m * n * k * 2 + 2 * m * k)/1e9
+    return (m * n * k * 2 + 2 * m * k) / 1e9
 
 
 def tune(inputs, pwr_limit=None, device=0):
-
     path = os.path.dirname(os.path.realpath(__file__)) + "/gemm/"
 
-    #n = np.int32(32)
-    #m = np.int32(16)
-    #k = np.int32(32)
+    # n = np.int32(32)
+    # m = np.int32(16)
+    # k = np.int32(32)
 
     total_flops = ops(*inputs)
 
     m, n, k = [np.int32(i) for i in inputs]
 
-    #// Matrices are accessed as follows:
-    #// A: [k*M + m], with 'k' ranging from 0:K and 'm' from 0:M (m,k,m)
-    #// B: [k*N + n], with 'k' ranging from 0:K and 'n' from 0:N (n,k,n)
-    #// C: [n*M + m], with 'n' ranging from 0:N and 'm' from 0:M (m,n,m)
+    # // Matrices are accessed as follows:
+    # // A: [k*M + m], with 'k' ranging from 0:K and 'm' from 0:M (m,k,m)
+    # // B: [k*N + n], with 'k' ranging from 0:K and 'n' from 0:N (n,k,n)
+    # // C: [n*M + m], with 'n' ranging from 0:N and 'm' from 0:M (m,n,m)
 
-    A = np.array(np.random.randn(m, k), order='F').astype(np.float32)
-    B = np.array(np.random.randn(k, n), order='F').astype(np.float32)
-    #C = np.array(np.random.randn(m, n), order='F').astype(np.float32)
-    C = np.zeros((m, n), order='F').astype(np.float32)
+    A = np.array(np.random.randn(m, k), order="F").astype(np.float32)
+    B = np.array(np.random.randn(k, n), order="F").astype(np.float32)
+    # C = np.array(np.random.randn(m, n), order='F').astype(np.float32)
+    C = np.zeros((m, n), order="F").astype(np.float32)
 
     alpha, beta = np.random.randn(2).astype(np.float32)
     alpha, beta = np.array([1.0, 1.0]).astype(np.float32)
 
     kernel_string = ""
-    files = ["common.opencl", "xgemm_part1.opencl", "xgemm_part2.opencl", "xgemm_part3.opencl"]
+    files = [
+        "common.opencl",
+        "xgemm_part1.opencl",
+        "xgemm_part2.opencl",
+        "xgemm_part3.opencl",
+    ]
     for f in files:
         with open(path + f, "r") as fp:
             kernel_string += fp.read()
@@ -84,24 +89,22 @@ def tune(inputs, pwr_limit=None, device=0):
     tune_params["SB"] = [0, 1]
     tune_params["PRECISION"] = [32]
 
-    #dryrun
-    #tune_params["MWG"] = [64, 128]
-    #tune_params["NWG"] = [64, 128]
-    #tune_params["KWG"] = [32]
-    #tune_params["MDIMC"] = [16]
-    #tune_params["NDIMC"] = [16]
-    #tune_params["MDIMA"] = [16]
-    #tune_params["NDIMB"] = [16]
-    #tune_params["KWI"] = [2]
-    #tune_params["VWM"] = [4]
-    #tune_params["VWN"] = [4]
-    #tune_params["STRM"] = [0]
-    #tune_params["STRN"] = [0]
-    #tune_params["SA"] = [1]
-    #tune_params["SB"] = [1]
-    #tune_params["PRECISION"] = [32]
-
-
+    # dryrun
+    # tune_params["MWG"] = [64, 128]
+    # tune_params["NWG"] = [64, 128]
+    # tune_params["KWG"] = [32]
+    # tune_params["MDIMC"] = [16]
+    # tune_params["NDIMC"] = [16]
+    # tune_params["MDIMA"] = [16]
+    # tune_params["NDIMB"] = [16]
+    # tune_params["KWI"] = [2]
+    # tune_params["VWM"] = [4]
+    # tune_params["VWN"] = [4]
+    # tune_params["STRM"] = [0]
+    # tune_params["STRN"] = [0]
+    # tune_params["SA"] = [1]
+    # tune_params["SB"] = [1]
+    # tune_params["PRECISION"] = [32]
 
     problem_size = (m, n)
 
@@ -120,14 +123,17 @@ def tune(inputs, pwr_limit=None, device=0):
 
     restrict += ["not (MWG == 128 and NWG == 128 and MDIMC == 8 and NDIMC == 8)"]
 
+    # observables = ["ps_energy", "ps_power"]
+    # ps_observer = PowerSensorObserver(observables, device="/dev/ttyACM0")
 
-    #observables = ["ps_energy", "ps_power"]
-    #ps_observer = PowerSensorObserver(observables, device="/dev/ttyACM0")
-
-    #nvmlobserver = NVMLObserver(["nvml_energy", "temperature", "core_freq", "mem_freq", "gr_voltage"],
+    # nvmlobserver = NVMLObserver(["nvml_energy", "temperature", "core_freq", "mem_freq", "gr_voltage"],
     #                             save_all=True, nvidia_smi_fallback="nvidia-smi", use_locked_clocks=True)
-    nvmlobserver = NVMLObserver(["nvml_energy", "temperature", "core_freq", "mem_freq"],
-                                 save_all=True, nvidia_smi_fallback="/cm/shared/package/utils/bin/run-nvidia-smi", use_locked_clocks=False)  #/cm/shared/package/utils/bin/run-nvidia-smi
+    nvmlobserver = NVMLObserver(
+        ["nvml_energy", "temperature", "core_freq", "mem_freq"],
+        save_all=True,
+        nvidia_smi_fallback="/cm/shared/package/utils/bin/run-nvidia-smi",
+        use_locked_clocks=False,
+    )  # /cm/shared/package/utils/bin/run-nvidia-smi
 
     metrics = get_metrics(total_flops)
     filename = "GEMM_NVML_" + device_name + "_" + energy_method
@@ -135,27 +141,40 @@ def tune(inputs, pwr_limit=None, device=0):
 
     start = time.time()
 
-    results, env = kernel_tuner.tune_kernel("Xgemm", kernel_string, problem_size, args, tune_params, block_size_names=block_size_names,
-                             lang="OpenCL", restrictions=restrict, verbose=False, compiler_options=["-I"+path],
-                             grid_div_x=grid_div_x, grid_div_y=grid_div_y,
-                             device=0, platform=0, iterations=72,
-                             observers=[nvmlobserver], metrics=metrics,
-                             cache=filename + "_cache.json")
+    results, env = kernel_tuner.tune_kernel(
+        "Xgemm",
+        kernel_string,
+        problem_size,
+        args,
+        tune_params,
+        block_size_names=block_size_names,
+        lang="OpenCL",
+        restrictions=restrict,
+        verbose=False,
+        compiler_options=["-I" + path],
+        grid_div_x=grid_div_x,
+        grid_div_y=grid_div_y,
+        device=0,
+        platform=0,
+        iterations=72,
+        observers=[nvmlobserver],
+        metrics=metrics,
+        cache=filename + "_cache.json",
+    )
 
     end = time.time()
-    env['execution_time'] = end-start
+    env["execution_time"] = end - start
 
-    with open(filename + "_output.json", 'w') as fh:
+    with open(filename + "_output.json", "w") as fh:
         json.dump(results, fh)
 
-    with open(filename + "_env.json", 'w') as fh:
+    with open(filename + "_env.json", "w") as fh:
         json.dump(env, fh)
 
     return results, env
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) > 1:
         pwr_limit_s = sys.argv[1]
         pwr_limit = int(pwr_limit_s)
@@ -166,4 +185,4 @@ if __name__ == "__main__":
     m = n = k = 4096
     device = 0
 
-    results, env = tune([m,n,k], pwr_limit=pwr_limit, device=device)
+    results, env = tune([m, n, k], pwr_limit=pwr_limit, device=device)
