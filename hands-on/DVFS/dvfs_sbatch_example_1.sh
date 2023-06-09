b2413f276cc3f36d51c29eb1a75d86ec40a8997f
Benjamin Czaja <benjamic@int4.local.snellius.surf.nl>
2023-06-09 13:27:40 +0200

    Cleaning up example1

--- hands-on/DVFS/dvfs_sbatch_example_1.sh
+++ hands-on/DVFS/dvfs_sbatch_example_1.sh
@@ -14,10 +14,13 @@ module load 2022
 module load foss/2022a
 module load AMD-uProf/4.0.341
 
-matrix_size=10000
 
-for i in {1500000..2600000..100000}
-do
-    echo "Launching power profiling @ Freq=$i"
-    srun --ntasks=1 --ear-cpufreq=$i --ear-policy=monitoring --ear-verbose=1 AMDuProfCLI timechart --event socket,power -o AMDuProf_output_power_freq_$i --interval 500 ../bin/mat_mul $matrix_size $matrix_size -p
-done
+matrix_size=10000
+# 2600000 is the nominal freq
+# you can choose from Freq from 1500000 to 2600000 in increments of 100000
+# in a more readable way (1.5 GHz, to 2.6 GHz in increments of 0.1 GHz)
+frequency=2600000
+
+echo "Launching power profiling @ Freq=$frequency"
+
+srun --ntasks=1 --ear-cpufreq=$frequency --ear-policy=monitoring --ear-verbose=1 AMDuProfCLI timechart --event socket,power -o AMDuProf_output_power_freq_$frequency --interval 500 ../bin/mat_mul $matrix_size $matrix_size -p
